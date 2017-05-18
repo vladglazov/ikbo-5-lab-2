9b40540f722abe330090b7fca8feb33dcbbc54b9
BlazingWizard <sanrise05@gmail.com>
2017-05-18 19:33:46 +0300

    my solution

---
+++ Lobanov/Function.h
@@ -0,0 +1,74 @@
+typedef struct DLLIST {
+	DLLIST *next;
+	DLLIST *prev;
+	int data;
+};
+
+DLLIST* DLCreate() {
+	DLLIST *NewItem;
+	NewItem = (DLLIST*)malloc(sizeof(DLLIST));
+	if (NewItem != NULL) {
+		NewItem->prev = NewItem->next = NULL;
+		NewItem->data = 0;
+	}
+	else {
+		printf("Eror.No memory\n");
+		return(NULL);
+	}
+	return(NewItem);
+}
+void DLAddBefore(DLLIST *ExitingItem) {
+	DLLIST *NewItem;
+	NewItem=DLCreate();
+	if (ExitingItem != NULL && NewItem != NULL) {
+		NewItem->next = ExitingItem;
+		NewItem->prev = ExitingItem->prev;
+		ExitingItem->prev = NewItem;
+	}
+	if (NewItem->prev != NULL) {
+		NewItem->prev->next = NewItem;
+	}
+	return;
+}
+void DLAddAfter(DLLIST *ExitingItem) {
+	DLLIST *NewItem;
+	NewItem = DLCreate();
+	if (ExitingItem != NULL && NewItem != NULL) {
+		NewItem->next = ExitingItem->next;
+		NewItem->prev = ExitingItem;
+		ExitingItem->next = NewItem;
+	}
+	if (NewItem->next != NULL) {
+		NewItem->next->prev = NewItem;
+	}
+	return;
+}
+void GetFirst(DLLIST *current){
+	while (current->prev != NULL)
+		current = current->prev;
+	return;
+}
+void DestroyDL(DLLIST *current) {
+	DLLIST *Next;
+	GetFirst(current);
+	while (current->next != NULL) {
+		Next = current->next;
+		free(current);
+	}
+	return;
+}
+char* delsp(char* buffer) {
+	int i, j = 0;
+	char command[255];
+	for (i = 0; buffer[i] != '\0'; i++)
+		if (buffer[i] != ' ' && buffer[i] != '\n' && buffer[i] != '\t') {
+			command[j] = buffer[i];
+			if (command[0] == '*') {
+				strcpy(command, "skip");
+				return(command);
+			}
+			j++;
+		}
+	command[j] = '\0';
+	return(command);
+}
---
+++ Lobanov/Source.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Function.h"
+int main(void) {
+
+	FILE *fp;
+	char buffer[255],*com;
+	long int b[10];
+	DLLIST *curr;
+	size_t i=-1;
+
+	fp = fopen("data.txt", "r");
+	curr=DLCreate();
+	
+	while (fgets(buffer, 256, fp)) {
+
+		com = delsp(buffer);
+		
+		if (!strcmp(com, "movl")) {
+			if (curr->prev == NULL) {
+				DLAddBefore(curr);
+				curr = curr->prev;
+			}
+			else
+				curr = curr->prev;
+			continue;
+		}
+
+		if (!strcmp(com, "movr")) {
+			if (curr->next == NULL) {
+				DLAddAfter(curr);
+				curr = curr->next;
+			}
+			else
+				curr = curr->next;
+			continue;
+		}
+		
+		if (!strcmp(com, "inc")) {
+			if (curr->data > 255) {
+				printf("Too big value. Can't increase.\n");
+				curr->data = 0;
+				continue;
+			}
+			curr->data++;
+			continue;
+		}
+
+		if (!strcmp(com, "dec")) {
+			if (curr->data == 0) {
+				printf("Too small value. Can't decrease.\n");
+				continue;
+			}
+			curr->data--;
+			continue;
+		}
+
+		if (!strcmp(com, "print")) {
+			printf("Your data: %d\n", curr->data);
+			continue;
+		}
+
+		if (!strcmp(com, "get")) {
+			printf("Enter data: ");
+			scanf("%d", &curr->data);
+			continue;
+		}
+
+		if (!strcmp(com, "printc")) {
+			printf("Your symbol: %c\n", curr->data);
+			continue;
+		}
+		
+		
+		if (!strcmp(com, "begin")) {
+			i++;
+			b[i] = ftell(fp);
+			if (i > 9) {
+				printf("Eror.Too much nesting\n");
+				break;
+			}
+			continue;
+		}
+
+		if (!strcmp(com, "end")) {
+			
+			if (i == -1) {
+				printf("Eror.Miss begin");
+				break;
+			}
+
+			if (curr->data != 0) {
+				fseek(fp, b[i], SEEK_SET);
+				continue;
+			}
+			i--;
+			continue;
+		}
+		
+		if (!strcmp(com, "skip")) {
+			continue;
+		}
+
+		printf("Invalid sintax\n");
+		break;
+	}
+	DestroyDL(curr);
+	return 0;
+}
+
